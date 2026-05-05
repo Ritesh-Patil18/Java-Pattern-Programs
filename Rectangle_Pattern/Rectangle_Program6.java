c83dd91838b45693b8f0ce0b1939b94e42007072
Ritesh-Patil18 <riteshdesale18@gmail.com>
2026-05-06 00:09:10 +0530

    Add Rectangle Pattern Programs

---
+++ Rectangle_Pattern/Rectangle_Program6.java
@@ -0,0 +1,15 @@
+class Rectangle_Program6
+{
+	public static void main (String args[])
+	{
+		char n='A';
+		for(char i='E'; i>=n; i--)
+		{
+			for(char j='A'; j<='E'; j++)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ Rectangle_Pattern/Rectangle_Program7.java
@@ -0,0 +1,17 @@
+class Rectangle_Program7
+{
+	public static void main( String args[])
+	{
+		char n='a';
+		for(char i='e'; i>='a'; i--)
+		{
+			for(char j=n; j<='e' ;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+}
+	
+			
