c1a031932e54457086428bd2b030dad6c0f18f41
Ritesh-Patil18 <riteshdesale18@gmail.com>
2026-05-07 23:58:51 +0530

    Added Space_Triangle_Programs

---
+++ Space_Triangle_Programs/Space_Triangle17.java
@@ -0,0 +1,27 @@
+/*
+     e
+    dd
+   ccc
+  bbbb
+ aaaaa
+*/
+
+class Space_Triangle17
+{
+	public static void main(String args[])
+	{
+		int n=8;
+		for(int i=n; i>=1; i--)
+		{	
+			for(int j=1; j<i; j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=i; j<=n; j++)
+			{	
+				System.out.print((char)(i+96));
+			}
+		 System.out.println();
+		}
+	}
+}
---
+++ Space_Triangle_Programs/Space_Triangle24.java
@@ -0,0 +1,28 @@
+class Space_Triangle24
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		for(int i=1; i<=n; i++)
+		 {
+			for(int j=1; j<i; j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=n; j>=i; j--)
+			{
+				System.out.print((char)(j+64));
+
+			}
+			System.out.println();
+		}
+	}
+}
+
+/*
+EDCBA
+ EDCB
+  EDC
+   ED
+    E
+*/
